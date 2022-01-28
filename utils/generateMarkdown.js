e6be88ef72e89f1eff4c9fe709892b9c56f7b597
jack-ie <jackie919@outlook.com>
2022-01-28 11:36:13 -0600

    markdown js uodate

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,5 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+//Create a function that returns a license badge based on which license is passed in
+//If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === 'Apache 2.0') {
     return `![Apache 2.0 License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`;
@@ -14,14 +14,33 @@ function renderLicenseBadge(license) {
   }
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) { }
+//Create a function that returns the license link
+//If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (license === "Apache 2.0") {
+    return `[Apache 2.0 License](https://opensource.org/licenses/Apache-2.0)`;
+  } else if (license === "BSD") {
+    return `[BSD (3-Clause) License](https://opensource.org/licenses/BSD-3-Clause)`;
+  } else if (license === "MIT") {
+    return `[MIT License](https://opensource.org/licenses/MIT)`;
+  } else if (license === "Unlicense") {
+    return `[Unlicense](http://unlicense.org/)`;
+  } else {
+    return "";
+  }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) { }
+//Create a function that returns the license section of README
+//If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (license === "No License") {
+    return "";
+  } else {
+    return `## License`, '<br>', `This project is licensed under the ${renderLicenseLink(license)}.`;
+  }
+}
 
+// Generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
@@ -49,10 +68,8 @@ function generateMarkdown(data) {
   ${data.usage}
   
   ## License
-  (https://img.shields.io/badge/license-${data.license}-blue.svg)
-  <br />
-  This application is covered by the ${data.license}license. 
-  
+  ${renderLicenseSection(data.license)}
+    
   ## Contributing
   ${data.contributing}
  
